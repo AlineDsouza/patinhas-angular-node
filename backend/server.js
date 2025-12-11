4a602a06eb6e386a58699e315eafae6ee2fc7c5d
Aline Silva <aline.dscsilva@gmail.com>
2025-12-11 13:12:25 +0000

    comentários no server.js

--- backend/server.js
+++ backend/server.js
@@ -1,60 +1,63 @@
-import express, { request, response } from 'express'; //Cria um servidor web para receber requisições HTTP GET, POST, DELETE, etc..
-import dotenv from 'dotenv'; // permite variáveis de ambiente
-import mongoose from 'mongoose'; // permite o q código converse com o banco de dados Mongodb
+import express, { request, response } from 'express'; 
+// cria um servidor web para receber requisições HTTP GET, POST, DELETE, etc..
+import dotenv from 'dotenv';
+// permite usar variáveis de ambiente
+import mongoose from 'mongoose';
+// permite o q código converse com o banco de dados Mongodb
 import cors from 'cors';
+// cors permite que o front consiga acessar essa API
 
-//CONFIGURAÇÃO DO EXPRESS
+// CONFIG DO EXPRESS
 dotenv.config(); //busca um arquivo env | entende o arquivo como variável de ambiente
 
 //ROTAS
-import petsRouter from './routes/petsRouters.js';
-import utilizadorRoutes from './routes/utilizadorRouters.js';
-import authRoutes from './routes/authRouters.js';
-import adocaoRouters from './routes/adocaoRouters.js';
+import petsRouter from './routes/petsRouters.js'; // rotas dos pets
+import utilizadorRoutes from './routes/utilizadorRouters.js';  // rotas dos utilizadores
+import authRoutes from './routes/authRouters.js'; // rotas de login e registo
+import adocaoRouters from './routes/adocaoRouters.js'; // rotas de adoção
 
 
-const app = express(); //variável app será nosso servidor principal
-const PORT = 3000; //Define a porta onde o servidor vai rodar ex: http://localhost:3000
-//app.use(express.json());  //Middleware - função que trata as informações recebidas - converte as informações p. json
-// aceita JSON grande, necessário para imagens 
-app.use(express.json({ limit: "15mb" }));
-app.use(express.urlencoded({ limit: "15mb", extended: true }));
+const app = express(); 
+// cria o servidor com express (variável app representa o servidor) 
+const PORT = 3000;
+//Define a porta onde o servidor vai rodar
 
-// Middlewares 
+
+app.use(express.json({ limit: "15mb" })); //entender dados enviados em JSON
+app.use(express.urlencoded({ limit: "15mb", extended: true })); //limite 15mb maior pq as imagens ocupam mais espaço
+//entender dados enviados em formulários, tipo quando você envia algo do HTML
+
+
+// CONFIG CORS
 app.use(cors({
-    origin: ['http://localhost:4200'], 
-  methods: ['GET','POST','PUT','DELETE','UPDATE'],
-  credentials: true
-}));              // Permite que front acesse a API
+    origin: ['http://localhost:4200'], // Permite que front acesse a API
+    methods: ['GET','POST','PUT','DELETE','UPDATE'], // métodos permitido
+    credentials: true// permite trabalhar com autenticação
+}));              
 
-
-//CONEXÃO COM MONGODB - função que tenta conectar ao banco de dados 
-const conectaDB = async () =>{
+//CONEXÃO COM MONGODB
+const conectaDB = async () =>{ // função que tenta conectar ao banco de dados 
     try{
-        await mongoose.connect(process.env.MONGO_URI); //process.env.MONGO_URI // Acessa as informações do arquivo .env
-        console.log('✅ Conectado ao MongoDB');
+        await mongoose.connect(process.env.MONGO_URI); 
+        // acessa as informações do arquivo .env (process.env.MONGO_URI)
+        console.log('Conectado ao MongoDB');
     } catch(error) {
-        console.log('❌ Erro ao conectar ao MongoDB', error.message);
+        console.log('Erro ao conectar ao MongoDB', error.message); 
+        // se der erro aparecerá isto aqui
     }
 }; 
 conectaDB();
+// chamando a função para conectar
 
-//CONEXÃO DAS ROTAS
+// CONEXÃO DAS ROTAS
+// defino qual arquivo cada rota deve usar
 app.use('/api/pets', petsRouter);
 app.use('/api/utilizador', utilizadorRoutes);
 app.use('/auth', authRoutes);
 app.use('/api/adocao', adocaoRouters);
 
-
-//INICIA O SERVIDOR e fica "escutando" na porta 3000 (definida na variável acima)
-app.listen(PORT, () => { //arrow funcion
-console.log(`O servidor está rodando na porta ${PORT} ✔ `); //mensagem de confirmação 
+// INICIA O SERVIDOR
+// fica "escutando" na porta 3000 (definida na variável acima)
+app.listen(PORT, () => {
+console.log(`O servidor está rodando na porta`);
 });
-
-
-
-
-
-
-
-
