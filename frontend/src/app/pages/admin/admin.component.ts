97b1aa1779c2bc929878ba21578c06f84f45cdeb
Aline Silva <aline.dscsilva@gmail.com>
2025-12-19 17:09:02 +0000

    implementa funcionalidade baixar pdf do formulário de adoção

--- frontend/src/app/pages/admin/admin.component.ts
+++ frontend/src/app/pages/admin/admin.component.ts
@@ -6,6 +6,8 @@ import { AdocaoForm } from '../../models/adocao-form';
 import { Router } from '@angular/router';
 import { NgFor, NgIf } from '@angular/common';
 import { HeaderComponent } from '../../core/components/header/header.component';
+import jsPDF from 'jspdf';
+
 
 @Component({
   selector: 'app-admin',
@@ -46,4 +48,43 @@ email: string | null = '';
   irParaCrud(): void {
     this.router.navigate(['/crud-pets']);
   }
+
+//Baixar pdf do formulário
+
+baixarPdf(formulario: AdocaoForm): void {
+  const doc = new jsPDF();
+
+  doc.setFontSize(16);
+  doc.text('Formulário de Adoção', 20, 20);
+
+  doc.setFontSize(12);
+
+  doc.text(`Nome do adotante: ${formulario.nomeAdotante}`, 20, 40);
+  doc.text(`Email: ${formulario.emailAdotante}`, 20, 50);
+  doc.text(`Telefone: ${formulario.telefone}`, 20, 60);
+
+  doc.text('Rotina do Pet:', 20, 80);
+  doc.text(formulario.rotinaPet, 20, 90, { maxWidth: 170 });
+
+  doc.text('Experiência Anterior com Animais:', 20, 120);
+  doc.text(formulario.experienciaAnterior, 20, 130, { maxWidth: 170 });
+
+  doc.text(
+    `Disponibilidade: ${formulario.disponibilidade}`,
+    20,
+    160
+  );
+
+  doc.text('Justificativa para adoção:', 20, 180);
+  doc.text(
+    formulario.justificativa,
+    20,
+    190,
+    { maxWidth: 170 }
+  );
+
+  doc.save(`formulario-adocao-${formulario.nomeAdotante}.pdf`);
+}
+
+
 }
