96168b70ed460988b7e8d54b8a45d3349cf4b6cd
Aline <aline.dscsilva@gmail.com>
2026-01-15 23:12:30 +0000

    comentários no código

--- frontend/src/app/services/adocao.service.ts
+++ frontend/src/app/services/adocao.service.ts
@@ -7,14 +7,21 @@ import { AdocaoForm } from '../models/adocao-form';
   providedIn: 'root'
 })
 export class AdocaoService {
+// URL da API responsável pelas operações de adoção
   private apiUrl = 'http://localhost:3000/api/adocao';
-
+// Injeta o HttpClient para permitir comunicação com o backend via HTTP
   constructor(private http: HttpClient) {}
 
+  // Envia os dados do formulário de adoção para o backend
+  // Utiliza o método POST para criar um novo registo de adoção
+  // Retorna um Observable com a resposta da API
    enviarFormulario(dados: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/formulario-adocao`, dados);
   }
-
+  
+// Obtém todos os formulários de adoção registados no sistema
+// Utiliza o método GET para consultar os dados no backend
+// Retorna um Observable com a lista de formulários
   buscarTodosFormularios(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/formularios`);
   }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -6,17 +6,20 @@ import { lastValueFrom, Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-
+// URL da API responsável pelas operações de adoção
   private apiUrl = 'http://localhost:3000/auth';
-  
 
-
+// Injeta o HttpClient para permitir comunicação HTTP com o backend
   constructor(private http: HttpClient) {}
+  
  /*  REGISTO  */
+// Envia os dados de registo do utilizador para o backend
+// Utiliza o método POST para criar um novo utilizador
   registrar(dados: any): Observable<any> {
     return this.http.post(`${this.apiUrl}/registo`, dados);
   }
  /*  LOGIN  */
+// Envia as credenciais de login para o backend
   async login(dados: any) {
     return await lastValueFrom(
       this.http.post(`${this.apiUrl}/login`, dados)
